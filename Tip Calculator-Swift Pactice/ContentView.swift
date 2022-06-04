f8d01dcf0e26882747e961e8a24c22624b2914e3
max-getlife <max.getlife@yahoo.com>
2022-06-04 19:02:15 +0300

    Initial Commit

---
+++ Tip Calculator-Swift Pactice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip Calculator-Swift Pactice
+//
+//  Created by Maksym Getlife on 04.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tip Calculator-Swift Pactice/Tip_Calculator_Swift_PacticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_Calculator_Swift_PacticeApp.swift
+//  Tip Calculator-Swift Pactice
+//
+//  Created by Maksym Getlife on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tip_Calculator_Swift_PacticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
